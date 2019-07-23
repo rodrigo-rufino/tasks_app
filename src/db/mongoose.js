5d384d2f3a25826a0ec944f65ad33147d4147203
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-23 15:00:35 -0300

    data validation and sanitization for task and user models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/';
 const databaseName = 'tasks_app_api';
@@ -10,29 +11,76 @@ mongoose.connect(connectionURL + databaseName, {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid e-mail');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 1,
+        validate(value) {
+            if (value < 0){
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Weak password! Contains the word "password'.);
+            }
+        }
     }
 });
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        required: false,
+        default: false
     }
 });
 
-const task = new Task({
-    description: 'Learn Mongoose',
-    completed: false
-});
 
-task.save().then(() => {
-    console.log(task);
-}).catch((error) => {
-    console.log(error);
-});
+
+
+// const user = new User({
+//     name: 'Rodrigo Rufino Ribeiro',
+//     email: 'valid@email.com',
+//     password: '123password4567'
+// });
+// user.save().then(() => {
+//     console.log(user);
+// }).catch((error) => {
+//     console.log(error);
+// });;
+
+// const task = new Task({
+//     description: 'Learn Mongoose',
+//     completed: false
+// });
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((error) => {
+//     console.log(error);
+// });
