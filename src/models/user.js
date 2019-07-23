cf28a3466ae1846d849c1dee041489be01974dda
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-23 16:39:32 -0300

    endpoints for create tasks and users

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid e-mail');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 1,
+        validate(value) {
+            if (value < 0){
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Weak password! Contains the word "password".');
+            }
+        }
+    }
+});
+
+
+module.exports = User;
