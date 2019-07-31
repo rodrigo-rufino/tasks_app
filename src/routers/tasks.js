a0308317239e7f583c9848103b1133c139665e84
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-07-31 14:40:06 -0300

    tasks pagination

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -18,6 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
 
@@ -30,7 +31,11 @@ router.get('/tasks', auth, async (req, res) => {
         // or
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate();
         res.send(req.user.tasks);
     } catch (error) {
